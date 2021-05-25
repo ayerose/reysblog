e9e145008b939d751d1c0b0d8d143ad4b342aa68
ayerose <reya-k@outlook.de>
2021-05-25 18:36:56 +0100

    update

--- home/models.py
+++ home/models.py
@@ -1,17 +1,15 @@
 from django.db import models
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
-
-
+from modelcluster.fields import ParentalKey
 
 class HomePage(Page):
     body = RichTextField(blank=True)
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
     ] 
-
     
 # BANNER
 
@@ -44,3 +42,19 @@ class HomePage(Page):
 
         verbose_name = "Home Page"
         verbose_name_plural = "Home Pages"
+        
+        
+# CAROUSEL ON HOMEPAGE
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [ImageChooserPanel("carousel_image")]
